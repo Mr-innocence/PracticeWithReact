231b3ff266d946de17b3f827e3b1a1aaa676f9d3
Mr-innocence <bellali1019@gmail.com>
2018-07-28 20:18:56 +1000

    basic feature

--- src/App.js
+++ src/App.js
@@ -4,14 +4,68 @@ import Person from './Person/Person';
 import Hobby from './Person/Hobby';
 
 class App extends Component {
+state = {
+  persons: [
+    {name: 'Jz', age : 25},
+    {name: 'ZK', age : 26},
+    {name: 'PP', age : 27},
+  ]
+}
+
+switchHandler = (newName) =>{
+  this.setState({
+    persons: [
+      {name: newName, age : 25},
+      {name: 'ZK', age : 26},
+      {name: 'PP', age : 30},
+    ]
+
+  })
+
+}
+
+changeHandler = (event) => {
+  this.setState({
+    persons: [
+      {name: event.target.value, age : 25},
+      {name: 'ZK', age : 26},
+      {name: 'PP', age : 30},
+    ]
+
+  })
+
+}
+
+
+
   render() {
+    const style = {
+      backgroundColor: 'White',
+      font: 'inherit',
+      margin: 'auto',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'        
+    }
     return (
+
       <div className = "APP">
-        <Person name = "JZ" age = "28"/>
-        <Person name = "ZK" age = "29">
+        <button 
+        style = {style}
+        onClick ={() => this.switchHandler('marshmallow')}>Switch Info</button>
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age}
+          change = {this.changeHandler} />
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age}>
           <Hobby hobby = "dancing" />
         </Person>
-        <Person name = "PP" age = "26"/>
+        <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age}
+          click = {this.switchHandler}/>
       </div>
     );
   }
--- src/Person/Hobby.js
+++ src/Person/Hobby.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+
+
+
+
+
 const hobby = (props) =>{
     return <p>My hobby is {props.hobby}</p>;
-
 }
 export default hobby;
